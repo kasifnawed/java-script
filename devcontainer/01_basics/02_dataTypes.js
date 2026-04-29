795ee9fc974de8d6c40c710893e1245573bfcae6
kasifnawed <kasifnawed0528@gmail.com>
2026-04-29 11:03:22 +0000

    dataTypes

---
+++ devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+// alert( 3+3 ) // we are using node JS not browser
+
+console.log(3 + 3)
+
+console.log("kasif")
+
+let name = "kasif"
+let age = 18
+let isLoggedIn = false
+
+
+//Number => 2 to power 53 
+// bigint 
+// String => ""
+// boolean => true/false
+// null
+// undefined =>
+    //symbol => unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
