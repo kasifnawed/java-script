94f25e3fa384e2367c13ae06992190f11443fccc
kasifnawed <kasifnawed0528@gmail.com>
2026-05-02 15:17:45 +0000

    Operation dataTypes

--- devcontainer/01_basics/03_conversionOperation.js
+++ devcontainer/01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "King"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 =>true; 0 => false
 //"" => false
@@ -25,5 +25,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+//******************Operation************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Kasif"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 2 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
