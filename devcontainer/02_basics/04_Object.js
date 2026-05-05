7cbc48e988f737bef49c8edbd0e1112b9671a9cc
kasifnawed <kasifnawed0528@gmail.com>
2026-05-05 10:47:53 +0000

    02_object

---
+++ devcontainer/02_basics/04_Object.js
@@ -0,0 +1,57 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+
+ tinderUser.id = "123abc"
+ tinderUser.name = "suar"
+ tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname:{
+         userfullname:{
+            firstname:"kasif",
+            lastname:"nawed"
+         }
+    }
+}
+// console.log(regularUser.fullname.userfullname);
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"e", 6:"f"}
+
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3 = {...obj1,...obj2,...obj4}
+// console.log(obj3);
+
+
+
+const users = [
+    {
+        id: 1,
+        email: "kasif@wfeo.com"
+
+    },
+    {
+        id: 1,
+        email: "kasif@wfeo.com"
+
+    },
+    {
+        id: 1,
+        email: "kasif@wfeo.com"
+
+    },
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
