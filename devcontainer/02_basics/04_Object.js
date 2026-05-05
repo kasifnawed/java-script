0bfba08c2e68222e36e4f1aa6c475efb7e6c0735
kasifnawed <kasifnawed0528@gmail.com>
2026-05-05 17:53:24 +0000

    json

--- devcontainer/02_basics/04_Object.js
+++ devcontainer/02_basics/04_Object.js
@@ -48,10 +48,26 @@ const users = [
     },
 ]
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hithesh"
+}
+
+// const {courseInstructor} = course
+const {courseInstructor:instructor} = course
+
+// console.log(courseInstructor); //Same value 
+console.log(instructor);
+
+
